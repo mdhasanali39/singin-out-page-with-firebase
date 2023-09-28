fa5841b7a4f4f834f1c369249567e7758d962f74
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-09-28 19:30:01 +0600

    Sign-out: added Sign out button to sign out by clicking the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css'
 import { app } from './Firebase/firebase.config'
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 
 function App() {
   const auth = getAuth(app);
@@ -9,7 +9,7 @@ function App() {
 
   const [users, setUsers] = useState(null);
 
-
+console.log(users);
  
 
   const handleSignInButtonClick = () =>{
@@ -23,11 +23,27 @@ function App() {
       })
   }
 
+  const handleSignOutButtonClick = () => {
+    signOut(auth)
+    .then(result =>{
+      setUsers(result)
+      console.log(result);
+      console.log('sign out succesfull');
+    }).catch(error => {
+      console.log(error);
+    })
+  }
 
   return (
     <>
       <div>
+       <div>
+        {users ? 
+          <button onClick={handleSignOutButtonClick}>Sign Out</button>
+          :
         <button onClick={handleSignInButtonClick}>Sign In with Google</button>
+        }
+        </div>
         { 
          users && <div>
           <h2>userName:{users.user.displayName}</h2>
